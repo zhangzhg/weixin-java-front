c7b5c561922f7b7c286384437516ddc7ed607e74
zhangzg <zhaoguo.zhang@e-vada.com>
2019-01-19 01:45:48 +0800

    菜单， elementui，vue升级

--- src/components/menu/MenuList.vue
+++ src/components/menu/MenuList.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="row mg">
     <div class="col-md-6">
-      <el-tree :data="menus" :props="props" show-checkbox default-expand-all ref="tree" @node-click="nodeClick"></el-tree>
+      <el-tree :data="menus" node-key="id" :props="props" show-checkbox default-expand-all ref="tree" @node-click="nodeClick" :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span class="right10">{{ node.label }}</span>
+          <span v-if="node.id!=1">
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => append(data)">
+              新增
+            </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => remove(node, data)">
+              删除
+            </el-button>
+          </span>
+        </span>
+      </el-tree>
     </div>
 
     <div class="col-md-6 box">
       <div class="row">
         <div class="col-md-6">
-          <v-input name="name" label="名称" validate="required" v-model="form.name"/>
-          <v-input name="authUrl" label="权限路径" v-model="form.authUrl"/>
+          <v-input name="name" type="input" label="名称" validate="required" v-model="form.name"/>
+          <v-input name="authUrl" type="input" label="权限路径" placeholder="parentCode/code/*" validate="required" v-model="form.authUrl"/>
+          <v-input type="radio" label="资源类型">
+            <div class="form_row">
+              <vuestic-radio-button name="type" class="right30" value="1" v-model="form.type" label="菜单" />
+              <vuestic-radio-button name="type" class="right30" value="2" v-model="form.type" label="功能" />
+            </div>
+          </v-input>
         </div>
         <div class="col-md-6">
-          <v-input name="authCode" label="编码" validate="required" v-model="form.authCode"/>
-          <v-input name="parentName" label="上级菜单" validate="required" v-model="form.parentName"/>
+          <v-input  name="authCode" type="input" label="编码" validate="required" v-model="form.authCode"/>
+          <v-input type="input" label="上级菜单"  v-model="form.parentName" isReadOnly/>
         </div>
+      </div>
+      <div class="row left10">
+        <button type="button" class="btn btn-primary btn-micro">保存</button>
       </div>
     </div>
   </div>
@@ -28,12 +55,12 @@ export default {
     return {
       menus: [
         {
-          id: 4,
-          label: '后台管理系统',
-          children: [{ id: 7, label: 'Cables' }, {
+          id: 1,
+          name: '后台管理系统',
+          children: [{ id: 7, name: 'Cables' }, {
             id: 8,
-            label: 'Cables',
-            children: [{ id: 7, label: 'Cables' }, { id: 8, label: 'Cables' }],
+            name: 'Cables',
+            children: [{ id: 9, name: 'Cables' }, { id: 10, name: 'Cables' }],
           }],
         },
       ],
@@ -47,14 +74,22 @@ export default {
       },
       props: {
         children: 'children',
-        label: 'label',
+        label: 'name',
       },
     }
   },
   methods: {
     nodeClick (node) {
+      this.form.name = node.name
+      this.form.authCode = String(node.id)
       console.log(node)
-    }
+    },
+    append (data) {
+      console.log(data)
+    },
+    remove (node, data) {
+      console.log(node)
+    },
   }
 }
 </script>
--- src/vuestic-theme/vuestic-components/v-input/VInput.vue
+++ src/vuestic-theme/vuestic-components/v-input/VInput.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="form-group with-icon-right" :class="{'has-error': errors.has(name)}">
-    <div class="input-group">
-      <label class="form-label" :for="name">{{label| translate}} </label>
-      <input :id="name" :name="name" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" v-validate="validate"/>
+    <div class="input-group" v-if="type==='input'" >
+      <label class="form-label">{{label| translate}} </label>
+      <input :id="name"
+             :name="name"
+             v-bind:value="value"
+             :placeholder="placeholder"
+             v-on:input="$emit('input', $event.target.value)"
+             v-validate="validate"
+             v-bind:readonly="isReadOnly"
+      />
       <i class="bar"></i>
       <i class="fa fa-exclamation-triangle error-icon icon-right input-icon"></i>
       <i class="fa fa-check valid-icon icon-right input-icon"></i>
       <small :v-show="errors.has(name)" class="icon-right10 help text-danger input-icon">{{ errors.first(name) }}</small>
+    </div>
+    <div class="input-group" v-else>
+      <label class="form-label">{{label| translate}} </label>
+      <slot />
+      <i class="bar"></i>
     </div>
   </div>
 </template>
@@ -23,19 +35,23 @@ export default {
       type: String,
       default: ''
     },
-    readonly: {
-      type: Boolean,
-      default: false
-    },
-    required: {
-      type: Boolean,
-      default: false
+    type: {
+      type: String,
+      default: ''
     },
     validate: {
       type: String,
       default: ''
     },
     value: {
+      type: String,
+      default: null
+    },
+    isReadOnly: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
       type: String,
       default: null
     }
